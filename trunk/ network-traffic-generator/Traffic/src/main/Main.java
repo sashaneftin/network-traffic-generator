926028d5d3e1096a42f032ecdd0678ebc4534b9d
sashaneftin <sashaneftin@02c2299e-95f4-11dd-b56f-139fe7fe61cf>
2008-10-11 20:22:27 +0000

    initial version

---
+++ trunk/ network-traffic-generator/Traffic/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		int a = 0xde;
+		
+		// TODO Auto-generated method stub
+		System.out.println(a);
+
+	}
+
+}
